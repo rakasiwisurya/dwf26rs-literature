01343094835037a478fca3dba1b26557fd390dd6
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-11-30 15:20:58 +0700

    done resolve getdetailcollection

--- src/pages/DetailLiterature.js
+++ src/pages/DetailLiterature.js
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
-import { FloatingLabel, Form } from "react-bootstrap";
 import { NotificationManager } from "react-notifications";
 import { pdfjs, Document, Page } from "react-pdf";
 
@@ -21,35 +20,28 @@ export default function DetailLiterature() {
 
   const [detailLiterature, setDetailLiterature] = useState(null);
   const [detailCollection, setDetailCollection] = useState(null);
-  const [collected, setCollected] = useState(false);
   const [show, setShow] = useState(false);
-  const [editable, setEditable] = useState(false);
-  const [form, setForm] = useState(null);
-  const [preview, setPreview] = useState(null);
 
   useEffect(() => {
     getDetailLiterature();
+    getDetailCollection();
   }, []);
+
+  const getDetailCollection = async () => {
+    try {
+      const response = await API.get(`/collections/literature/${id}`);
+      setDetailCollection(response.data.data);
+    } catch (error) {
+      if (error.response) {
+        setDetailCollection(null);
+      }
+    }
+  };
 
   const getDetailLiterature = async () => {
     try {
       const response = await API.get(`/literatures/${id}`);
       setDetailLiterature(response.data.data);
-      setForm(response.data.data);
-
-      const collections = await API.get(`/collections/${state.user?.id}`);
-      const myCollect = collections.data.data.filter(
-        (item) => item.literature.id === response.data.data.id
-      );
-      setDetailCollection(myCollect[0]);
-
-      for (const myCollection of collections.data.data) {
-        if (myCollection.literature.id === response.data.data.id) {
-          setCollected(true);
-        } else {
-          setCollected(false);
-        }
-      }
     } catch (error) {
       console.log(error);
     }
@@ -57,7 +49,7 @@ export default function DetailLiterature() {
 
   const handleCollect = async () => {
     try {
-      if (collected) {
+      if (detailCollection) {
         const response = await API.delete(
           `/collections/${detailCollection.id}`
         );
@@ -65,11 +57,14 @@ export default function DetailLiterature() {
         if (response.status === 200) {
           NotificationManager.success(
             response.data.message,
-            response.data.status
+            response.data.status,
+            4000,
+            () => {
+              history.push("/collection");
+            }
           );
 
-          setCollected(false);
-          getDetailLiterature();
+          getDetailCollection();
         }
       } else {
         const config = {
@@ -95,8 +90,7 @@ export default function DetailLiterature() {
             }
           );
 
-          setCollected(true);
-          getDetailLiterature();
+          getDetailCollection();
         }
       }
     } catch (error) {
@@ -121,86 +115,6 @@ export default function DetailLiterature() {
     }
   };
 
-  const handleChange = (e) => {
-    setForm((prevState) => ({
-      ...prevState,
-      [e.target.id]:
-        e.target.type === "file" ? e.target.files[0] : e.target.value,
-    }));
-
-    if (e.target.type === "file") {
-      const fileList = e.target.files;
-
-      for (const file of fileList) {
-        setPreview(URL.createObjectURL(file));
-      }
-    }
-  };
-
-  const handleUpdate = async () => {
-    try {
-      let response;
-
-      if (typeof form.attache === "object") {
-        const config = {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        };
-
-        const formData = new FormData();
-        formData.set("attache", form.attache, form.attache.name);
-        formData.set("title", form.title);
-        formData.set("userId", form.userId);
-        formData.set("publication_date", form.publication_date);
-        formData.set("pages", form.pages);
-        formData.set("isbn", form.isbn);
-        formData.set("author", form.author);
-
-        response = await API.put(
-          `/literature/${detailLiterature?.id}`,
-          formData,
-          config
-        );
-      } else {
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-
-        const body = JSON.stringify({
-          ...form,
-          attache: form.attache.split("/")[5],
-        });
-
-        response = await API.put(
-          `/literature/${detailLiterature?.id}`,
-          body,
-          config
-        );
-      }
-
-      if (response.status === 200) {
-        NotificationManager.success(
-          response.data.message,
-          response.data.status
-        );
-
-        getDetailLiterature();
-        setEditable(false);
-      }
-    } catch (error) {
-      console.log(error);
-      if (error?.response.data?.message) {
-        return NotificationManager.error(
-          error.response.data.message,
-          error.response.data.status
-        );
-      }
-    }
-  };
-
   const handleDownload = () => {
     donwloadPdf(detailLiterature.attache, String(detailLiterature.title));
     NotificationManager.success("Download starting...", "Success");
@@ -215,16 +129,12 @@ export default function DetailLiterature() {
           <div className="row">
             <div
               className="col-auto position-relative"
-              style={editable ? {} : { cursor: "pointer" }}
-              onClick={
-                editable
-                  ? undefined
-                  : () => {
-                      window.open(detailLiterature?.attache);
-                    }
-              }
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                window.open(detailLiterature?.attache);
+              }}
             >
-              <Document file={preview ? preview : detailLiterature?.attache}>
+              <Document file={detailLiterature?.attache}>
                 <Page
                   pageNumber={1}
                   width={295}
@@ -232,205 +142,75 @@ export default function DetailLiterature() {
                   className="rounded"
                 />
               </Document>
-              {editable && (
-                <div className="pdf-edit">
-                  <input
-                    type="file"
-                    name="attache"
-                    id="attache"
-                    aria-label="file upload"
-                    onChange={handleChange}
-                    hidden
-                  />
-                  <label htmlFor="attache" className="btn btn-info text-light">
-                    Upload New PDF
-                  </label>
-                </div>
-              )}
             </div>
             <div className="col ps-5">
-              {editable ? (
-                <>
-                  <FloatingLabel
-                    className="mb-3"
-                    controlId="title"
-                    label="Title"
-                  >
-                    <Form.Control
-                      type="text"
-                      placeholder="title"
-                      onChange={handleChange}
-                      value={form.title}
-                    />
-                  </FloatingLabel>
-                  <FloatingLabel
-                    className="mb-3"
-                    controlId="author"
-                    label="Author"
-                  >
-                    <Form.Control
-                      type="text"
-                      placeholder="author"
-                      onChange={handleChange}
-                      value={form.author}
-                    />
-                  </FloatingLabel>
-                  <FloatingLabel
-                    className="mb-3"
-                    controlId="publication_date"
-                    label="Publication Date"
-                  >
-                    <Form.Control
-                      type="date"
-                      placeholder="publication_date"
-                      onChange={handleChange}
-                      value={form.publication_date}
-                    />
-                  </FloatingLabel>
-                  <FloatingLabel
-                    className="mb-3"
-                    controlId="pages"
-                    label="Pages"
-                  >
-                    <Form.Control
-                      type="number"
-                      placeholder="pages"
-                      onChange={handleChange}
-                      value={form.pages}
-                    />
-                  </FloatingLabel>
-                  <FloatingLabel className="mb-3" controlId="isbn" label="ISBN">
-                    <Form.Control
-                      type="number"
-                      placeholder="isbn"
-                      onChange={handleChange}
-                      value={form.isbn}
-                    />
-                  </FloatingLabel>
-                </>
-              ) : (
-                <>
-                  <div className="mb-4">
-                    <h1 className="h3 fw-bold">{detailLiterature?.title}</h1>
-                    <div className="text-muted" style={{ fontSize: 20 }}>
-                      {detailLiterature?.author}
-                    </div>
-                  </div>
+              <div className="mb-4">
+                <h1 className="h3 fw-bold">{detailLiterature?.title}</h1>
+                <div className="text-muted" style={{ fontSize: 20 }}>
+                  {detailLiterature?.author}
+                </div>
+              </div>
 
-                  <div className="mb-4">
-                    <div className="fw-bold tag-title">Publication Date</div>
-                    <div className="text-muted tag-data">
-                      {detailLiterature?.author}
-                    </div>
-                  </div>
+              <div className="mb-4">
+                <div className="fw-bold tag-title">Publication Date</div>
+                <div className="text-muted tag-data">
+                  {detailLiterature?.author}
+                </div>
+              </div>
 
-                  <div className="mb-4">
-                    <div className="fw-bold tag-title">Pages</div>
-                    <div className="text-muted tag-data">
-                      {detailLiterature?.pages}
-                    </div>
-                  </div>
+              <div className="mb-4">
+                <div className="fw-bold tag-title">Pages</div>
+                <div className="text-muted tag-data">
+                  {detailLiterature?.pages}
+                </div>
+              </div>
 
-                  <div className="mb-4">
-                    <div className="fw-bold tag-title">ISBN</div>
-                    <div className="text-muted tag-data">
-                      {detailLiterature?.isbn}
-                    </div>
-                  </div>
+              <div className="mb-4">
+                <div className="fw-bold tag-title">ISBN</div>
+                <div className="text-muted tag-data">
+                  {detailLiterature?.isbn}
+                </div>
+              </div>
 
-                  <div className="tag">
-                    <button
-                      className="btn btn-danger py-2"
-                      onClick={handleDownload}
-                    >
-                      Download
-                      <span className="ms-3">
-                        <i className="fas fa-cloud-download-alt"></i>
-                      </span>
-                    </button>
-                  </div>
-                </>
-              )}
+              <div className="tag">
+                <button
+                  className="btn btn-danger py-2"
+                  onClick={handleDownload}
+                >
+                  Download
+                  <span className="ms-3">
+                    <i className="fas fa-cloud-download-alt"></i>
+                  </span>
+                </button>
+              </div>
             </div>
             <div className="col-auto">
               {state.user?.id === detailLiterature?.user.id ? (
-                <div className="d-flex flex-column">
-                  {editable ? (
-                    <>
-                      <button
-                        className="btn btn-success mb-3"
-                        onClick={handleUpdate}
-                      >
-                        Submit
-                        <span>
-                          <i className="fas fa-check ms-2"></i>
-                        </span>
-                      </button>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => {
-                          setEditable(false);
-                        }}
-                      >
-                        Cancel
-                        <span>
-                          <i className="fas fa-ban ms-2"></i>
-                        </span>
-                      </button>
-                    </>
-                  ) : (
-                    <>
-                      <button
-                        className="btn btn-warning text-light mb-3"
-                        onClick={() => {
-                          setEditable(true);
-                        }}
-                      >
-                        Edit Literature
-                        <span>
-                          <i className="fas fa-edit ms-2"></i>
-                        </span>
-                      </button>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => {
-                          setShow(true);
-                        }}
-                      >
-                        Delete Literature
-                        <span>
-                          <i className="fas fa-trash ms-2"></i>
-                        </span>
-                      </button>
-                    </>
-                  )}
-                </div>
+                <button className="btn btn-secondary" disabled>
+                  You are the owner
+                  <span>
+                    <i class="fas fa-user-tag  ms-2"></i>
+                  </span>
+                </button>
               ) : (
                 <>
-                  {!collected ? (
-                    <>
-                      <button
-                        className="btn btn-danger"
-                        onClick={handleCollect}
-                      >
-                        Add My Collection
-                        <span>
-                          <i className="far fa-bookmark ms-2"></i>
-                        </span>
-                      </button>
-                    </>
+                  {!detailCollection ? (
+                    <button className="btn btn-danger" onClick={handleCollect}>
+                      Add My Collection
+                      <span>
+                        <i className="far fa-bookmark ms-2"></i>
+                      </span>
+                    </button>
                   ) : (
-                    <>
-                      <button
-                        className="btn btn-secondary"
-                        onClick={handleCollect}
-                      >
-                        Remove Collection
-                        <span>
-                          <i className="fas fa-bookmark ms-2"></i>
-                        </span>
-                      </button>
-                    </>
+                    <button
+                      className="btn btn-secondary"
+                      onClick={handleCollect}
+                    >
+                      Remove Collection
+                      <span>
+                        <i className="fas fa-bookmark ms-2"></i>
+                      </span>
+                    </button>
                   )}
                 </>
               )}
